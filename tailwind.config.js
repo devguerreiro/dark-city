79da81ca50fa1e1fb31dcd1a3743a5ebc7bf6bf9
Luis <luiscvlh11@gmail.com>
2021-11-15 17:21:27 -0300

    Added tailwind's custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        dark: 'var(--color-dark)',
+        light: 'var(--color-light)',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
