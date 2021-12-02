1adf35c6485e06a92e83695dbe944ad4f12f0357
Luis <luiscvlh11@gmail.com>
2021-12-01 23:56:57 -0300

    Save score on localStorage

--- src/models/character.ts
+++ src/models/character.ts
@@ -22,17 +22,39 @@ export class Character implements ICharacter {
 
   public changeScore(index: number, value: boolean): void {
     this.score.splice(index, 1, value);
+
+    this.updateScoreOnLocalStorage(this.score);
   }
 
   public resetScore(): void {
     const hasAtLeastOneScore = this.score.some((s) => s);
 
     if (hasAtLeastOneScore) {
-      this.score = this.getDefaultScore();
+      this.score = this.getEmptyScore();
+
+      this.updateScoreOnLocalStorage(this.score);
     }
   }
 
-  private getDefaultScore(): TScore {
+  private getEmptyScore(): TScore {
     return Array(maxScore).fill(false);
+  }
+
+  private getDefaultScore(): TScore {
+    const defaultScore = localStorage.getItem(this.name);
+
+    if (!defaultScore) {
+      const score = this.getEmptyScore();
+
+      this.updateScoreOnLocalStorage(score);
+
+      return score;
+    }
+
+    return JSON.parse(defaultScore);
+  }
+
+  private updateScoreOnLocalStorage(score: TScore): void {
+    localStorage.setItem(this.name, JSON.stringify(score));
   }
 }
